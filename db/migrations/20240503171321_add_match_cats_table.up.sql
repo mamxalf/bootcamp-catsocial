5e9ebaaa16ee817fa248d52c7c2b4989d28158bc
Hammam Firdaus <hammamxalf@gmail.com>
2024-05-04 00:20:34 +0700

    create matches migration

---
+++ db/migrations/20240503171321_add_match_cats_table.up.sql
@@ -0,0 +1,22 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS matches (
+                                    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), -- uuid v4
+                                    issued_user_id UUID NOT NULL,
+                                    match_cat_d UUID NOT NULL,
+                                    user_cat_id UUID NOT NULL,
+                                    message TEXT,
+                                    is_approved boolean NOT NULL DEFAULT FALSE,
+
+                                    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+-- create trigger trigger for automatically set updated_at on row update
+CREATE TRIGGER set_matches_updated_at BEFORE UPDATE ON matches FOR EACH ROW EXECUTE PROCEDURE set_updated_at();
+
+-- alter table
+ALTER TABLE matches ADD FOREIGN KEY (issued_user_id) REFERENCES users (id);
+ALTER TABLE matches ADD FOREIGN KEY (match_cat_d) REFERENCES cats (id);
+ALTER TABLE matches ADD FOREIGN KEY (user_cat_id) REFERENCES cats (id);
+
+COMMIT;
